4972a91de0122cdf882a204da7ec08c904ed66b9
theophiluskibet <kibettheophilus@gmail.com>
2025-11-20 13:34:07 +0300

    setup http client

--- app/src/main/java/com/theophiluskibet/coopbank/di/AppModule.kt
+++ app/src/main/java/com/theophiluskibet/coopbank/di/AppModule.kt
@@ -2,8 +2,43 @@ package com.theophiluskibet.coopbank.di
 
 import com.theophiluskibet.coopbank.data.remote.BankApi
 import com.theophiluskibet.coopbank.data.remote.BankApiImpl
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.DefaultRequest
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.header
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.URLProtocol
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
+
+const val BASE_URL = "card-services.free.beeceptor.com"
 
 val appModule = module {
     single<BankApi> { BankApiImpl(httpClient = get()) }
+    singleOf(::createHttpClient)
 }
+
+private fun createHttpClient(engine: HttpClientEngine = OkHttp.create()) =
+    HttpClient(engine) {
+        install(ContentNegotiation) {
+            json(
+                Json {
+                    ignoreUnknownKeys = true
+                    isLenient = true
+                },
+            )
+        }
+
+        install(DefaultRequest) {
+            url {
+                host = BASE_URL
+                protocol = URLProtocol.HTTPS
+            }
+            header(HttpHeaders.ContentType, ContentType.Application.Json)
+        }
+    }
